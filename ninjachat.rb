c4c50a462af5b71f2e8bd6f57e8e285e365ef995
realonecan <151053518+realonecan@users.noreply.github.com>
2025-05-11 13:14:05 +0500

    Update ninjachat.rb
    Fix sha256 for v1.1.0 ninja binary

--- ninjachat.rb
+++ ninjachat.rb
@@ -2,7 +2,7 @@ class Ninjachat < Formula
   desc "Anonymous chat client"
   homepage "https://github.com/realonecan/ninja"
   url "https://github.com/realonecan/ninja/releases/download/v1.1.0/ninja-darwin-amd64.tar.gz"
-  sha256 "..."  # ← paste output from `shasum -a 256 ninja-darwin-amd64.tar.gz`
+  sha256 "0003cb444629161e5d8d35040751954be28dd1501a775c1322f668498c8898a2"  # ← paste output from `shasum -a 256 ninja-darwin-amd64.tar.gz`
   version "1.1.0"
 
   def install
