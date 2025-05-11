c7db8589568d257e11e1fc562ed6b3ccec309f09
realonecan <151053518+realonecan@users.noreply.github.com>
2025-05-11 15:14:29 +0500

    Update ninjachat.rb

--- ninjachat.rb
+++ ninjachat.rb
@@ -1,9 +1,9 @@
 class Ninjachat < Formula
   desc "Anonymous chat client"
   homepage "https://github.com/realonecan/ninja"
-  url "https://github.com/realonecan/ninja/releases/download/v1.1.0/ninja-darwin-amd64-v1.1.0.tar.gz"
-  sha256 "b55676ff077e3e365aaa04ac594b89c31df0847e926b9dcb49ff468f698d87df"
-  version "1.1.0"
+  url "https://github.com/realonecan/ninja/releases/download/v1.2.0/ninja-darwin-amd64-v1.2.0.tar.gz"
+  sha256 "89fb8b8f3af798595a0684897e28cdd69cddb1e0691b1166292de5090b3f5bcd"
+  version "1.2.0"
 
   def install
     bin.install "ninja"
