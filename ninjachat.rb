e40e8e4f90b89da7e21f07bd05c05fc606a8dffe
realonecan <151053518+realonecan@users.noreply.github.com>
2025-05-11 12:44:41 +0500

    Update ninjachat.rb

--- ninjachat.rb
+++ ninjachat.rb
@@ -1,16 +1,15 @@
 class Ninjachat < Formula
   desc "Anonymous chat client"
   homepage "https://github.com/realonecan/ninja"
-  url "https://github.com/realonecan/ninja/releases/download/v1.0.0/ninja-darwin-amd64.tar.gz"
-  sha256 "3d6a4ae7f9c973c26ef3f706c22c04e9875aff3a93d5ec6dd9ebde3aed95dbc3"
-  version "1.0.0"
+  url "https://github.com/realonecan/ninja/releases/download/v1.1.0/ninja-darwin-amd64.tar.gz"
+  sha256 "..."  # ← paste output from `shasum -a 256 ninja-darwin-amd64.tar.gz`
+  version "1.1.0"
 
-def install
-  bin.install "ninja"
-end
+  def install
+    bin.install "ninja"
+  end
 
   test do
-    # Since ninja doesn't have a --version flag, let's just check if the binary exists
     assert_predicate bin/"ninja", :exist?
   end
 end
